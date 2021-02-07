e6de86a3d9f470c0ed1f6611e2eea97a6398759d
yoyo860222 <75553513+yoyo860222@users.noreply.github.com>
2021-02-07 17:02:25 +0800

    角色前後左右移動

---
+++ 3D_FPS_Unity/Assets/Script/FPSController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class FPSController : MonoBehaviour
+{
+    [Header("移動速度"), Range(0, 2000)]
+    public float speed;
+    [Header("旋轉"), Range(0, 2000)]
+    public float turn;
+
+    private Animator ani;
+    private Rigidbody rig;
+
+    private void Awake()
+    {
+        ani = GetComponent<Animator>();
+        rig = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+
+        rig.MovePosition(transform.position + transform.forward * v * speed * Time.deltaTime +  transform.right * h * speed * Time.deltaTime);
+
+    }
+
+
+}
