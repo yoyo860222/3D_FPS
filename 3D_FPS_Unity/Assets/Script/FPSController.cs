fbc16f3002b91390fa6e6a0d05721dd3261ec9a5
yoyo860222 <75553513+yoyo860222@users.noreply.github.com>
2021-02-21 13:04:01 +0800

    開槍製作中

--- 3D_FPS_Unity/Assets/Script/FPSController.cs
+++ 3D_FPS_Unity/Assets/Script/FPSController.cs
@@ -2,7 +2,7 @@
 
 public class FPSController : MonoBehaviour
 {
-    
+    #region 控制宣告
     [Header("移動速度"), Range(0, 2000)]
     public float speed;
     [Header("旋轉"), Range(0, 2000)]
@@ -13,6 +13,21 @@ public class FPSController : MonoBehaviour
     public Vector3 floorOffset;
     [Header("地板偵測半徑"), Range(0, 2000)]
     public float floorRaduis = 1;
+    #endregion
+
+    #region 開槍宣告
+    [Header("子彈生成位置")]
+    public Transform pointFire;
+    [Header("子彈")]
+    public GameObject bullet;
+    [Header("子彈目前數量")]
+    public int bulletCurrent = 30;
+    [Header("子彈總數")]
+    public int bulletTotal = 150;
+    [Header("子彈速度")]
+    public int bulletSpeed = 1000;
+    #endregion
+
 
     private Animator ani;
     private Rigidbody rig;
@@ -34,6 +49,16 @@ public class FPSController : MonoBehaviour
     {
         Move();
         Jump();
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            GameObject temp = Instantiate(bullet, pointFire.position, pointFire.rotation);
+            temp.GetComponent<Rigidbody>().AddForce(pointFire.forward * bulletSpeed);
+        }
     }
 
     private void Jump()
